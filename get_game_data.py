0a1e7f69f17c40461d09f01d542a3bfc9fdb94f6
Kirill David Sorokin <96061027+Kirill-Sorokin@users.noreply.github.com>
2024-03-25 18:51:34 +0000

    create method that finds the/all game paths.

--- get_game_data.py
+++ get_game_data.py
@@ -1,8 +1,24 @@
 import os, json, shutil, sys
 from subprocess import PIPE, run
 
+
+GAME_DIR_PATTERN = "game"
+
+def find_all_game_paths(source):
+    game_paths = []
+    for root, dirs, files in os.walk(source):
+        for directory in dirs:
+            if GAME_DIR_PATTERN in directory.lower():
+                path = os.path.join(source, directory)
+                game_paths.append(path)
+        break
+    return game_paths
+
 def main(source, target):
-    pass
+    cwd = os.getcwd()
+    # "C://..." + .... <- 'String concatenation': different OS have different path dividers
+    source_path = os.path.join(cwd, source)
+    target_path = os.path.join(cwd, target)
 
 if __name__ == "__main__":
     args = sys.argv
